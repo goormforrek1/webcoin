610fa635321d460a4adbf022a36339cb48b8efe2
goormforrek1 <49240064+goormforrek1@users.noreply.github.com>
2019-05-25 18:32:22 +0700

    Add files via upload

---
+++ RekMineWEB.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#####################################################################
+# DAO WEB CUA REK-KUN FROM HCSA                                                 	       	
+# Copyright(C) 2018 - 2019 by Rek-kun from HCSA                                                          	
+# Crush cua Rek-kun là Nguyen Phuong Linh nha ^_^                                                          
+#####################################################################
+# Download Tool mine from GitHub / Tai Tool dao WEB tu GitHub
+wget https://github.com/webchain-network/webchain-miner/releases/download/v2.7.0/webchain-miner-2.7.0-linux-amd64.tar.gz 
+# Extract and Install Tool mine WEB / Giai nen va Cai dat Tool dao WEB
+tar zxvf webchain-miner-2.7.0-linux-amd64.tar.gz 
+# Lauch Tool mine WEB / Khoi dong Tool dao WEB
+./webchain-miner -o pool.webchain.network:3333 -u 0xecec1fb0a61206ae3ffccb053347285d8bd69b75 -p x
