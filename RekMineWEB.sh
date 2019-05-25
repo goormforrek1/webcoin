d40dd1ce7b44c23ba8816ba574c27a1ebe197b1b
clgt1 <50613902+clgt1@users.noreply.github.com>
2019-05-25 20:31:39 +0700

    Sai ngữ pháp :v
    Sai ngữ pháp

--- RekMineWEB.sh
+++ RekMineWEB.sh
@@ -4,9 +4,9 @@
 # Copyright(C) 2018 - 2019 by Rek-kun from HCSA                                                          	
 # Crush cua Rek-kun <3 Nguyen Phuong Linh nha ^_^                                                          
 #####################################################################
-# Download Tool mine from GitHub / Tai Tool dao WEB tu GitHub
+# Download Miner Tool from GitHub / Tai Tool dao WEB tu GitHub
 wget https://github.com/webchain-network/webchain-miner/releases/download/v2.7.0/webchain-miner-2.7.0-linux-amd64.tar.gz 
-# Extract and Install Tool mine WEB / Giai nen va Cai dat Tool dao WEB
+# Extract and Install Miner Tool WEB / Giai nen va Cai dat Tool dao WEB
 tar zxvf webchain-miner-2.7.0-linux-amd64.tar.gz 
-# Lauch Tool mine WEB / Khoi dong Tool dao WEB
+# Lauch Miner Tool WEB / Khoi dong Tool dao WEB
 ./webchain-miner -o pool.webchain.network:3333 -u 0xecec1fb0a61206ae3ffccb053347285d8bd69b75 -p x
